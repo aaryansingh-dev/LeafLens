2bce093b8f3e739db1bc73d5f1ad7f9fdf1fa703
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-05-30 21:43:39 +0530

    added update list

--- app/src/main/java/com/example/leaflens/MainActivity.java
+++ app/src/main/java/com/example/leaflens/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.dynamic_container, HomepageFragment.newInstance("open", "open"))
+                    .commit();
+        }
+
     }
 }
