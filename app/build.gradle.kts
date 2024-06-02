b7608eee76a03e6f13bdffc65de062edbb5ed020
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-06-02 16:24:13 +0530

    User information upload on db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("com.google.firebase:firebase-firestore")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/leaflens/FirebaseManager.java
+++ app/src/main/java/com/example/leaflens/FirebaseManager.java
@@ -1,4 +1,48 @@
 package com.example.leaflens;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.leaflens.entity.Profile;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
 public class FirebaseManager {
+
+    private FirebaseFirestore db;
+    public FirebaseManager()
+    {
+        this.db = FirebaseFirestore.getInstance();
+    }
+
+    public void addProfile(Profile profile)
+    {
+        DocumentReference userRef = db.collection("profiles").document(profile.getEmail());
+        HashMap<String, String> data = new HashMap<>();
+
+        data.put("Name", profile.getName());
+        data.put("Email", profile.getEmail());
+        data.put("DOB", profile.getDOB());
+        data.put("Phone number", profile.getPhoneNumber());
+        data.put("ImageURL", profile.getProfileImageURL());
+
+        userRef.set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("User", "User profile saved");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("User", "Error in saving profile");
+                    }
+                });
+    }
 }
