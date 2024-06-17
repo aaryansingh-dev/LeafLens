9d1489dd0b7e2c6528a7d2ed75ce1651259dc2ce
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-06-17 12:04:07 +0530

    Create Disease.java

---
+++ app/src/main/java/com/example/leaflens/entity/Disease.java
@@ -0,0 +1,64 @@
+package com.example.leaflens.entity;
+
+import java.util.ArrayList;
+
+public class Disease {
+
+    private String name;
+    private String severity;
+    private ArrayList<String> symptoms;
+    private ArrayList<String> treatments;
+
+    public Disease(String name, String severity)
+    {
+        this.name = name;
+        this.severity = severity;
+    }
+
+    public Disease(String name, String severity, ArrayList<String> symptoms, ArrayList<String> treatments)
+    {
+        this.name = name;
+        this.severity = severity;
+        this.symptoms = symptoms;
+        this.treatments = treatments;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setSeverity(String severity)
+    {
+        this.severity = severity;
+    }
+
+    public String getSeverity()
+    {
+        return this.severity;
+    }
+
+    public void setSymptoms(ArrayList<String> symptoms)
+    {
+        this.symptoms = symptoms;
+    }
+
+    public ArrayList<String> getSymptoms()
+    {
+        return this.symptoms;
+    }
+
+    public void setTreatments(ArrayList<String> treatments)
+    {
+        this.treatments = treatments;
+    }
+
+    public ArrayList<String> getTreatments(){
+        return this.treatments;
+    }
+}
