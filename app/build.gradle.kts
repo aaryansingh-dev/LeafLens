1d04ece08db610ea955685cde84260b350510f9d
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-06-19 23:15:46 +0530

    minor changes to Settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ dependencies {
     implementation ("androidx.activity:activity:1.9.0")
     implementation("androidx.core:core:1.13.1")
     implementation ("androidx.fragment:fragment:1.8.0")
+
+    implementation ("com.makeramen:roundedimageview:2.3.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
